6ac8a38569c9539640a6b2f5d7cc4f6798fc9d79
Jeff Inman <jeffery.inman@hotmail.com>
2020-12-12 15:04:21 -0800

    app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+// console.log("hello, world") ;
+//console log();
+
+//camel case
+var myVariableName;
+
+var myVariableName = "hello, world!";
+
+console.log(myVariableName);
+
+var order; //walk to coffee shop
+order = "black coffee" ; //pace order
+
+console.log("Your order of "+ order + " is ready!") //order ready
+
+console.log(myVariableName);
+
+
+var pokemon = prompt();
