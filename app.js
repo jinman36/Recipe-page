ea30161760113ba6a0db39d32cf2e60d4b4b9f7f
Jeff Inman <jeffery.inman@hotmail.com>
2020-12-12 15:42:15 -0800

    added notes

--- app.js
+++ app.js
@@ -16,4 +16,15 @@ console.log("Your order of "+ order + " is ready!") //order ready
 console.log(myVariableName);
 
 
-var pokemon = prompt();
+var promptQuestion = "What is the best starter Pokemon?";
+var response = prompt(promptQuestion);
+console.log(starterPokemon);
+
+if (response === "Squirtle") {
+    console.log("excellent choice!")
+} else if (response === "bulbasar") {
+    console.log("I mean ok.. sure.");
+else {
+    console.log("no, you are worng.")
+
+}
