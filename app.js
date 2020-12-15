987ebf7f885c548eb98324af03e5ba42c2720442
Jeff Inman <jeffery.inman@hotmail.com>
2020-12-14 22:19:01 -0800

    modified but nothing is working for function

--- app.js
+++ app.js
@@ -1,37 +1,19 @@
 
 var promptQuestion = "For a BBQ, would you chose Steak, Hamburgers, or something different?";
 var response = prompt(promptQuestion);
-//console.log (response);
-//var reflected = response;
-//var textColor  = response;
-var backgroundColor;
-//var textSize  = response;
-//var alertMessage  = response;
+var Color = response;
 
+function addLogicBelow (response) {
 
-function addLogicBelow (response){
-
-if (response === "steak") {
-    //alertMessage = "Excellent!"
-    //reflected = "excellent choice! - Check the schedule below to make sure you are here on the right day!"
-    //textSize = "xx-large"
-    backgroundColor = "aqua"
-} else if (response === "hamburgers") {
-    //console.log = (response)
-    //alertMessage = "Check out page for better options!"
-    //reflected = "You should try steak - Be sure to check schedule below!";
-    //textColor = "#00FF00";
-    backgroundColor = "blue"
-    
-}else {
-    //alertMessage = "Check out page for better options!"
-    //reflected = "Did you misspell steak? If not, look below for our meal specials!"
-    //textColor = "#FF0000";
-    backgroundColor = "black";
+    if (response === "steak") {
+        color = "aqua"
+    } else if (response === "hamburgers") {
+        color = "blue"
+    } else {
+        color = "black";
+    }
 }
-
-    document.getElementById('response').style.backgroundColor = backgroundColor;
-}
+    document.getElementById("content").style.backgroundColor = color;
 
 addLogicBelow (response);
 
