3894ab442a4004431a6a16624ddc7618c24832c5
Jeff Inman <jeffery.inman@hotmail.com>
2020-12-22 22:16:12 -0800

    updated greeting

--- app.js
+++ app.js
@@ -95,3 +95,22 @@ function myMessage(response) {
 //}
 
 //testValidate()
+
+var today = new Date ();
+var hourNow = today.getHours();
+var greetings;
+
+
+function timedGreeting(){
+if (hourNow > 18) {
+    greetings - 'Salamat Malam!';
+} else if (hourNow > 12) {
+    greetings = 'Selamat Sore!';
+} else if (hourNow > 0) {
+    greetings = 'Selamat Pagi!';
+} else {
+    greetings = 'Selamat!';
+}
+}
+
+timedGreeting()
